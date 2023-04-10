8b7e2b51b5527ea06e58fa8b6f4d08aea358cde5
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-04-10 17:10:41 +0200

    Update README.md

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from './component/App';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 ReactDOM.render(
         <App />,
   document.getElementById("root")
